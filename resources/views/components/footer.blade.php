df57caa682091d587a243f8d951f516051eab1f8
aqsari <9d.aqsari.nufikha.putri.05@gmail.com>
2024-07-30 20:26:35 +0700

    update header and footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,5 @@
-<div class="flex item-center">
-    <p>Copyright 2024</p>
+<div class="flex  bg-white shadow">
+    <div class="object-center text-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h4 class="text-3 font-mono tracking-tight text-gray-900 ">Copyright 2024</h4>
+      </div>
 </div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,6 @@
-<header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $slot }}</h1>
+<header class="flex bg-white shadow ">
+    <div class="object-center text-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900 ">SISTEM PENDUKUNG KEPUTUSAN  PENENTUAN PPPK</h1>
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">KEMENTRIAN AGAMA KABUPATEN TANAH DATAR</h1>
     </div>
   </header>
