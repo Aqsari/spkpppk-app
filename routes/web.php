03ca18f288823a06cd17787aff04bf06ede160f1
aqsari <9d.aqsari.nufikha.putri.05@gmail.com>
2024-07-30 10:50:34 +0700

    setup awal dan add file view alternatif, bobot, hasil, home, login, perhitungan

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/alternatif', function () {
+    return view('alternatif');
+});
+
+Route::get('/bobot', function () {
+    return view('bobot');
+});
+
+Route::get('/perhitungan', function () {
+    return view('perhitungan');
+});
+
+Route::get('/hasil', function () {
+    return view('hasil');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
